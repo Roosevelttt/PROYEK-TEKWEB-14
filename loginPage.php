d61882db1f92ccc17b4261ae98c7e17216c62c88
Andika Pramana <andikap805@gmail.com>
2024-12-20 03:00:26 +0700

    ubah logo di login page

--- loginPage.php
+++ loginPage.php
@@ -181,15 +181,21 @@
         .text-behind:last-child {
             transform: translateY(50%);
         }
+
+        .responsive-image {
+        width: 100%; /* Gambar akan mengikuti lebar kontainer */
+        height: auto; /* Tinggi gambar disesuaikan secara proporsional */
+        max-width: 100%; /* Tidak akan melampaui lebar kontainer */
+        display: block; /* Menghindari jarak tambahan */
+        }
+
     </style>
 </head>
 
 <body>
     <div class="login-box">
         <div class="center-content">
-            <div class="text-behind">Hartono</div>
-            <div class="text-behind">Collections</div>
-            <div class="sphere"></div>
+            <img src="assets/logo6trsnprnt_1.png" alt="Deskripsi gambar" class="responsive-image">
         </div>
         <form id="login-form">
             <div class="user-box">
